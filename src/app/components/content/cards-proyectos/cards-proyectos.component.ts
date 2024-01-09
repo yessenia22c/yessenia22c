cadf4edbb2374d63ff4223d7619285d5e4676a7d
yessenia22c <yesseniavillarte@gmail.com>
2024-01-09 15:23:21 -0400

    estilos mejorado de cards y proyecto nuevo

--- src/app/components/content/cards-proyectos/cards-proyectos.component.ts
+++ src/app/components/content/cards-proyectos/cards-proyectos.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './cards-proyectos.component.css'
 })
 export class CardsProyectosComponent {
+  constructor() {}
+  cardFlipped: boolean = false;
+  voltearCard() {
+    this.cardFlipped = !this.cardFlipped;
 
+  }
+  linkSisicap() {
+    window.open('https://sistema.eliteiso.org', '_blank');
+  }
+  linkTienda() {
+    window.open('https://www.eliteiso.org/cursos', '_blank');
+  }
 }
