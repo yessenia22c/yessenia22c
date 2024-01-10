6dd3a4b2f52e2c9423851a4714013fb4c8b18bc5
yessenia22c <yesseniavillarte@gmail.com>
2024-01-10 15:57:48 -0400

    Proyectos terminado inicio de Experiencia

--- src/app/components/content/cards-proyectos/cards-proyectos.component.ts
+++ src/app/components/content/cards-proyectos/cards-proyectos.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cards-proyectos',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './cards-proyectos.component.html',
   styleUrl: './cards-proyectos.component.css'
 })
 export class CardsProyectosComponent {
   constructor() {}
   cardFlipped: boolean = false;
-  voltearCard() {
-    this.cardFlipped = !this.cardFlipped;
+  // voltearCard() {
+  //   this.cardFlipped = !this.cardFlipped;
 
+  // }
+
+
+  tarjetas = Array(3).fill({ isFlipped: false }); // Cambia el número 3 según el número de tarjetas
+
+  flipCard(index: number) {
+    this.tarjetas[index].isFlipped = !this.tarjetas[index].isFlipped;
   }
   linkSisicap() {
     window.open('https://sistema.eliteiso.org', '_blank');
@@ -20,4 +30,23 @@ export class CardsProyectosComponent {
   linkTienda() {
     window.open('https://www.eliteiso.org/cursos', '_blank');
   }
+
+  cardFlipped1: boolean = false;
+  cardFlipped2: boolean = false;
+  cardFlipped3: boolean = false;
+  cardFlipped4: boolean = false;
+
+  voltearCard(elemento: number) {
+  // Dependiendo del elemento, cambia la variable correspondiente
+  if (elemento === 1) {
+    this.cardFlipped1 = !this.cardFlipped1;
+  } else if (elemento === 2) {
+    this.cardFlipped2 = !this.cardFlipped2;
+  }else if (elemento === 3) {
+    this.cardFlipped3 = !this.cardFlipped3;
+  }else if (elemento === 4) {
+    this.cardFlipped4 = !this.cardFlipped4;
+  }
+  // Agrega lógica adicional si es necesario
+}
 }
--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CardsProyectosComponent } from "./cards-proyectos/cards-proyectos.component";
+import { ExperienciaComponent } from "./experiencia/experiencia.component";
 
 @Component({
     selector: 'app-content',
     standalone: true,
     templateUrl: './content.component.html',
     styleUrl: './content.component.css',
-    imports: [CardsProyectosComponent]
+    imports: [CardsProyectosComponent, ExperienciaComponent]
 })
 export class ContentComponent {
 
