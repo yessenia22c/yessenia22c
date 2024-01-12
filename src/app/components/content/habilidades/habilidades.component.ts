dc21f4057321225b698a7009560cfba9c3e23bc8
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-01-11 21:50:33 -0400

    Habilidades terminado

--- src/app/components/content/habilidades/habilidades.component.ts
+++ src/app/components/content/habilidades/habilidades.component.ts
@@ -9,46 +9,15 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class HabilidadesComponent implements AfterViewInit {
   @ViewChild('slider') slider!: ElementRef;
-
-  currentIndex = 0;
-  totalSlides!: number;
+  @ViewChild('logosSlide') logosSlide!: ElementRef;
+  @ViewChild('logos') logos!: ElementRef;
 
   ngAfterViewInit() {
-    this.totalSlides = this.slider.nativeElement.querySelectorAll('.slide').length;
-    console.log(this.totalSlides);
+    this.cloneLogosSlide();
   }
 
-  showSlide(index: number) {
-    const newPosition = -index * 100 + '%';
-    this.slider.nativeElement.style.transition = 'transform 0.5s ease-in-out';
-    this.slider.nativeElement.style.transform = 'translateX(' + newPosition + ')';
-  }
-
-  prevSlide() {
-    this.currentIndex = (this.currentIndex - 1 + this.totalSlides) % this.totalSlides;
-    this.showSlide(this.currentIndex);
-  }
-
-  nextSlide() {
-    this.currentIndex = (this.currentIndex + 1) % this.totalSlides;
-    this.showSlide(this.currentIndex);
-  }
-
-  resetSlider() {
-    this.currentIndex = 0;
-    this.showSlide(this.currentIndex);
-  }
-
-  transitionEnd() {
-    // Resetea la posición sin animación cuando la transición ha terminado
-    this.slider.nativeElement.style.transition = 'none';
-
-    // Si llegamos al último slide, retrocede al primero sin animación
-    if (this.currentIndex === this.totalSlides - 1) {
-      setTimeout(() => {
-        this.currentIndex = 0;
-        this.showSlide(this.currentIndex);
-      });
-    }
-  }
+  cloneLogosSlide() {
+     const copy = this.logosSlide.nativeElement.cloneNode(true);
+     this.logos.nativeElement.appendChild(copy);
+ }
 }
