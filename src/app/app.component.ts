9095996f84f733396685bc2982541443fac71ed3
yessenia22c <yesseniavillarte@gmail.com>
2024-01-13 12:42:39 -0400

    fuentes cambiadas foter y contacto colocado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { ContentComponent } from "./components/content/content.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, HeaderComponent, ContentComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, ContentComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'portafolio-v17-yessi';
--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CardsProyectosComponent } from "./cards-proyectos/cards-proyectos.component";
 import { ExperienciaComponent } from "./experiencia/experiencia.component";
 import { HabilidadesComponent } from "./habilidades/habilidades.component";
+import { ContactoComponent } from "./contacto/contacto.component";
 
 @Component({
     selector: 'app-content',
     standalone: true,
     templateUrl: './content.component.html',
     styleUrl: './content.component.css',
-    imports: [CardsProyectosComponent, ExperienciaComponent, HabilidadesComponent]
+    imports: [CardsProyectosComponent, ExperienciaComponent, HabilidadesComponent, ContactoComponent]
 })
 export class ContentComponent {
 
