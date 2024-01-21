38671cae34892c4290303e4bb1baa26f68e27b30
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-01-20 22:41:00 -0400

    arreglado header y botones optimizado y descarga de cv listo

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -3,6 +3,8 @@ import { CardsProyectosComponent } from "./cards-proyectos/cards-proyectos.compo
 import { ExperienciaComponent } from "./experiencia/experiencia.component";
 import { HabilidadesComponent } from "./habilidades/habilidades.component";
 import { ContactoComponent } from "./contacto/contacto.component";
+import { HttpClient } from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 @Component({
     selector: 'app-content',
@@ -12,9 +14,20 @@ import { ContactoComponent } from "./contacto/contacto.component";
     imports: [CardsProyectosComponent, ExperienciaComponent, HabilidadesComponent, ContactoComponent]
 })
 export class ContentComponent {
+    constructor(private http: HttpClient) {}
 
     abrirCorreo(){
         window.open('mailto:yesseniavillarte@gmail.com', '_blank');
     }
+    downloadCv(){
+        const pdfUrl = 'assets/document/CV-Yessenia_Villarte.pdf';
 
+    this.http.get(pdfUrl, { responseType: 'arraybuffer' })
+      .subscribe((data: ArrayBuffer) => {
+        const blob = new Blob([data], { type: 'application/pdf' });
+        saveAs(blob, 'CV-Yessenia-Villarte.pdf');
+      }, error => {
+        console.error('Error al descargar el PDF', error);
+      });
+    }
 }
